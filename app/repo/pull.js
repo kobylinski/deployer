a8229d1ef5a2bcc6e30e0ed8e61fe96b369ed009
Marek Kobylinski <marek@kobylinski.co>
2019-04-05 11:53:00 +0200

    [d:production]

--- app/repo/pull.js
+++ app/repo/pull.js
@@ -4,6 +4,6 @@ const repoBranch = require('./branch.js');
 module.exports = async (repo) => {
 	const branch = await repoBranch(repo);
 	const commit = await repo.mergeBranches(branch.local, branch.remote);
-	console.log(commit);
+	console.log(typeof commit, commit.toString);
 	return commit.tostrS ? commit.tostrS() : commit;
 };
