e82157e8275bc21fea2c7377462310071f6c491c
Marek Kobylinski <marek@kobylinski.co>
2019-04-05 10:07:05 +0200

    webhook [d:production]

--- app/repo/pull.js
+++ app/repo/pull.js
@@ -3,6 +3,6 @@ const repoBranch = require('./branch.js');
 
 module.exports = async (repo) => {
 	const branch = await repoBranch(repo);
-	const commit await repo.mergeBranches(branch.local, branch.remote);
+	const commit = await repo.mergeBranches(branch.local, branch.remote);
 	return commit.tostrS();
 };
